8d818452f4008b12479e95321d52e3f3b7dbe918
Kush Pandya <kushpandya111@gmail.com>
2021-06-08 19:58:07 -0600

    added header and sign-in link with custom form and button

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            rePassword: ''
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
